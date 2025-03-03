c8326fd55f699c60560dfd7c04a548af9fb43b66
R.Moroizumi <m.ryousuke0401@icloud.com>
2025-03-04 04:10:04 +0900

    ✨ Add social media links and icons to footer

--- src/components/_layout/footer/index.tsx
+++ src/components/_layout/footer/index.tsx
@@ -18,7 +18,12 @@ export default function Footer() {
               最新の技術トレンドと深い洞察を提供する技術ブログ
             </p>
             <div className="flex space-x-4">
-              <Link href="#" className="text-gray-500 hover:text-black">
+              <Link
+                href="https://x.com/mororyou"
+                className="text-gray-500 hover:text-black"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <span className="sr-only">Twitter</span>
                 <svg
                   className="h-6 w-6"
@@ -29,7 +34,12 @@ export default function Footer() {
                   <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
                 </svg>
               </Link>
-              <Link href="#" className="text-gray-500 hover:text-black">
+              <Link
+                href="https://github.com/mororyou"
+                className="text-gray-500 hover:text-black"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <span className="sr-only">GitHub</span>
                 <svg
                   className="h-6 w-6"
@@ -44,24 +54,39 @@ export default function Footer() {
                   />
                 </svg>
               </Link>
-              <Link href="#" className="text-gray-500 hover:text-black">
-                <span className="sr-only">LinkedIn</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+              <Link
+                href="https://zenn.dev/m_ryosuke"
+                className="text-gray-500 hover:text-black"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="sr-only">Zenn</span>
+                <Image
+                  src="/assets/images/sns/zenn.svg"
+                  alt="Zenn"
+                  className="text-gray-500 hover:text-black"
+                  width={24}
+                  height={24}
+                />
+              </Link>
+              <Link
+                href="https://qiita.com/mororyou"
+                className="rounded-full bg-gray-500 hover:text-black"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="sr-only">Qiita</span>
+                <Image
+                  src="/assets/images/sns/qiita.png"
+                  alt="Qiita"
+                  width={24}
+                  height={24}
+                />
               </Link>
             </div>
           </div>
-          {/* <div>
+          {/*
+          <div>
             <h3 className="mb-4 text-lg font-semibold">カテゴリー</h3>
             <ul className="space-y-2">
               <li>
@@ -150,7 +175,8 @@ export default function Footer() {
                 </Link>
               </li>
             </ul>
-          </div> */}
+          </div>
+          */}
         </div>
         <div className="mt-12 border-t border-gray-200 pt-8 text-center text-gray-500">
           <p>
